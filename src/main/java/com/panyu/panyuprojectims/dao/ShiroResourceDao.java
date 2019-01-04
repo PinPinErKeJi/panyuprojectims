3a35097ae4ba0d863d4640aab49df47284d3c1ae
zhangxiaoliang <py_zhangxiaoliang@163.com>
2019-01-04 11:33:47 +0800

    2019/1/4 提交  shiro dao接口添加

---
+++ src/main/java/com/panyu/panyuprojectims/dao/ShiroResourceDao.java
@@ -0,0 +1,9 @@
+package com.panyu.panyuprojectims.dao;
+import com.panyu.panyuprojectims.entity.ShiroResource;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface ShiroResourceDao{
+	
+}
---
+++ src/main/java/com/panyu/panyuprojectims/dao/ShiroRoleDao.java
@@ -0,0 +1,9 @@
+package com.panyu.panyuprojectims.dao;
+import com.panyu.panyuprojectims.entity.ShiroRole;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface ShiroRoleDao{
+	
+}
