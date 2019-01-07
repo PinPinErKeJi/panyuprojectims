8e6f5add7411c805049b95d7f80644d0b987e310
yangqi <py_yangqi@163.com>
2019-01-07 15:38:43 +0800

    2019-1-7 创建login.js

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,152 @@
+//为表单的必填文本框添加提示信息（选择form中的所有后代input元素）
+$("form :input.required").each(function () {
+    //通过jquery api：$("HTML字符串") 创建jquery对象
+    var $required = $("<strong class='high'>*</strong>");
+    //添加到this对象的父级对象下
+    $(this).parent().append($required);
+});
+
+//为表单元素添加失去焦点事件
+$("form :input").blur(function(){
+//               var $parent = $(this).parent();
+    //$parent.find(".msg").remove(); //删除以前的提醒元素（find()：查找匹配元素集中元素的所有匹配元素）
+    //验证公司名
+    if($(this).is("#companyName")){
+        var nameVal = $.trim(this.value); //原生js去空格方式：this.replace(/(^\s*)|(\s*$)/g, "")
+        var regCompany = /[~#^$@%&!*()<>:;'"{}【】  ]/;
+        if(nameVal == "" || nameVal.length < 6 ||
+            regCompany.test(nameVal)){
+            var errorMsg = " 公司名非空，长度6位以上，不包含特殊字符！";
+            //class='msg onError' 中间的空格是层叠样式的格式
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证用户名
+    if($(this).is("#userName")){
+        var userVal = $.trim(this.value);
+        //原生js去空格方式：this.replace(/(^\s*)|(\s*$)/g, "")
+        var regUser = /[~#^$@%&!*()<>:;'"{}【】  ]/;
+        if(userVal == "" || userVal.length < 2 ||
+            regUser.test(userVal)){
+            var errorMsg = " 用户名非空，长度2位以上，不包含特殊字符！";
+            //class='msg onError' 中间的空格是层叠样式的格式
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证工号
+    if($(this).is("#companyNumber")){
+        var numVal = $.trim(this.value);
+        //原生js去空格方式：this.replace(/(^\s*)|(\s*$)/g, "")
+        var regNum = /^\d{18}$/;
+        if(numVal == "" ||
+            regUserNum.test(numVal)){
+            var errorMsg = " 请输入工号！";
+            //class='msg onError' 中间的空格是层叠样式的格式
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证邮箱
+    if($(this).is("#email")){
+        var emailVal = $.trim(this.value);
+        var regEmail = /.+@.+\.[a-zA-Z]{2,4}$/;
+        if(emailVal== "" || (emailVal != "" && !regEmail.test(emailVal))){
+            var errorMsg = " 请输入E-Mail！";
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证密码
+    if($(this).is("#password")){
+        var pwdVal = $.trim(this.value);
+        var regPwd = /^[A-Za-z0-9]+$ 或 ^[A-Za-z0-9]{6,8}$/;
+        if(pwdVal== "" || (pwdVal != "" && !regPwd.test(pwdVal))){
+            var errorMsg = " 请输入密码！";
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //确认密码
+    if($(this).is("#surePassword")){
+        var sureVal = $.trim(this.value);
+        if(sureVal != pwdVal){
+            var errorMsg = " 密码不一致！";
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证手机号
+    if($(this).is("#tel")){
+        var telVal = $.trim(this.value);
+        var regTel = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+        if(telVal== "" || (telVal != "" && !regTel.test(telVal))){
+            var errorMsg = " 请输入手机号！";
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+    //验证码
+    if($(this).is("#code")){
+        var codeVal = $.trim(this.value);
+        var regCode = /.+@.+\.[a-zA-Z]{2,4}$/;
+        if(codeVal== "" || (codeVal != "" && !regCode.test(codeVal))){
+            var errorMsg = " 请输入验证码！";
+            $parent.append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+        else{
+            var okMsg=" 输入正确";
+            $parent.find(".high").remove();
+            $parent.append("<span class='msg onSuccess'>" + okMsg + "</span>");
+        }
+    }
+}).keyup(function(){
+    //triggerHandler 防止事件执行完后，浏览器自动为标签获得焦点
+    $(this).triggerHandler("blur");
+}).focus(function(){
+    $(this).triggerHandler("blur");
+});
+
+//点击重置按钮时，通过trigger()来触发文本框的失去焦点事件
+$("#send").click(function(){
+    //trigger 事件执行完后，浏览器会为submit按钮获得焦点
+    $("form .required:input").trigger("blur");
+    var numError = $("form .onError").length;
+    if(numError){
+        return false;
+    }
+    alert("注册成功！");
+});
+
+//用户协议
+
+
