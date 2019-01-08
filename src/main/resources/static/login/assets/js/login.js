b81e34607f158634eee36bc68b5c3a041c753fb1
pinpin <18671095993@163.com>
2019-01-08 09:50:25 +0800

    1.7加了login.js

---
+++ src/main/resources/static/login/assets/js/login.js
@@ -0,0 +1,21 @@
+
+    $(function () {
+        var formData=new FormData($("#Reform")[0]);
+        $("#register").click( function () {
+           alert(123);
+            $.ajax({
+                url:'userLoginController/userlogin',
+                type:'post',
+                data:formData,
+                secureuri: false,
+                async: false,
+                cache: false,
+                contentType: false,
+                processData: false,
+                dataType: 'json',
+                success:function(){
+
+                }
+            });
+        });
+    });
