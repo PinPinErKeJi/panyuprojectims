a866f322349c2d1ddfedd73e4d9275057a596227
pinpin <18671095993@163.com>
2019-01-09 14:59:36 +0800

    提交了UserService

--- src/main/java/com/panyu/panyuprojectims/service/PanyuUserService.java
+++ src/main/java/com/panyu/panyuprojectims/service/PanyuUserService.java
@@ -1,6 +1,17 @@
 package com.panyu.panyuprojectims.service;
 import java.util.List;
 import com.panyu.panyuprojectims.entity.PanyuUser;
+import org.apache.ibatis.annotations.Param;
+
 public interface PanyuUserService{
-	
+    public int register(   @Param("userProvince")String userProvince,
+                           @Param("userCity")String userCity,
+                           @Param("userCounty")String userCounty,
+                           @Param("userCompanyName")String userCompanyName,
+                           @Param("userCompanycccNumber")String userCompanycccNumber,
+                           @Param("userName")String userName,
+                           @Param("userEmail")String userEmail,
+                           @Param("userTel")String userTel,
+                           @Param("userPwd")String userPwd
+                           );
 }
