c801bf3767c58ae36dbbe1eb5595addd888f481e
pinpin <18671095993@163.com>
2019-01-09 15:03:09 +0800

    login.js提交

--- src/main/resources/static/login/js/login.js
+++ src/main/resources/static/login/js/login.js
@@ -1,21 +1,28 @@
 
     $(function () {
-        var formData=new FormData($("#Reform")[0]);
-        $("#register").click( function () {
-           // alert(123);
-            $.ajax({
-                url:'userLoginController/userlogin',
-                type:'post',
-                data:formData,
-                secureuri: false,
-                async: false,
-                cache: false,
-                contentType: false,
-                processData: false,
-                dataType: 'json',
-                success:function(){
+     // var formData=new FormData($("#Reform")[0]);
 
-                }
+        $("#register").click( function () {
+
+            var companyName=$("#companyName").val();
+            var userName=$("#userName").val();
+            var companyNumber=$("#companyNumber").val();
+            var email=$("#email").val();
+            var password=$("#password").val();
+            var tel=$("#tel").val();
+            var province=$("#province").val();
+            var city=$("#city").val();
+            var county=$("#county").val();
+
+
+            $.post("userLoginController/userlogin",
+                {"userCompanyName":companyName,"userName":userName,"userCompanycccNumber":companyNumber,"userEmail":email,"userPwd":password,"userTel":tel,
+                "userProvince":province,"userCity":city,"userCounty":county},
+                function(msg){
+                    if(msg>0){
+                        alert("注册成功！请点击返回登录");
+                        window.location.href="login.html";
+                    }
             });
         });
     });
