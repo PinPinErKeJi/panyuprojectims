677eb79489a3bdad33c7a4fb5a277166b6afdbed
pinpin <18671095993@163.com>
2019-01-10 11:20:01 +0800

    myShiroRealmt提交1.10

--- src/main/java/com/panyu/panyuprojectims/shiroRealm/MyShiroRealm.java
+++ src/main/java/com/panyu/panyuprojectims/shiroRealm/MyShiroRealm.java
@@ -1,13 +1,19 @@
 package com.panyu.panyuprojectims.shiroRealm;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.panyu.panyuprojectims.entity.PanyuUser;
+import com.panyu.panyuprojectims.service.PanyuUserService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
 
 public class MyShiroRealm extends AuthorizingRealm {
+    @Autowired
+    private PanyuUserService panyuUserService;
     /**授权**/
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
@@ -19,8 +25,26 @@ public class MyShiroRealm extends AuthorizingRealm {
     /**认证**/
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //获取界面上输入的当前登录人的信息（用户名密码）
+        UsernamePasswordToken curtoken= (UsernamePasswordToken) token;
+        String userName=curtoken.getUsername();
+        //根据当前登录的用户名从数据库查询，获取用户对象
+        PanyuUser panyuUser= panyuUserService.queryInfoByUsername(userName);
 
-        return null;
+        if(panyuUser==null){
+            System.out.println("用户名不正确");
+            return null;
+        }else{
+            String uuserName =panyuUser.getUserName();
+            String password=panyuUser.getUserPwd();
+
+            //System.out.println(uuserName);
+            //盐值
+            ByteSource salt=ByteSource.Util.bytes(userName);
+            return  new SimpleAuthenticationInfo(uuserName,password,salt,getName());
+
+        }
+
     }
 
 }
