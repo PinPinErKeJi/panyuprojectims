7070a27ec44b921bd65fa6c853e3d5fcfa1f393a
xiaodu <py_dujianlei@163.com>
2019-01-11 15:35:14 +0800

    2019-1-11提交判断注册controller控制类

--- src/main/java/com/panyu/panyuprojectims/controller/Login_controller/A_RegisterPanyuUserController.java
+++ src/main/java/com/panyu/panyuprojectims/controller/Login_controller/A_RegisterPanyuUserController.java
@@ -1,7 +1,8 @@
 package com.panyu.panyuprojectims.controller.Login_controller;
 
-import com.panyu.panyuprojectims.entity.PanyuUser;
 import com.panyu.panyuprojectims.service.A_RegisterPanyuUserService;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,8 @@ public class A_RegisterPanyuUserController {
     @ResponseBody
     public Map selectUserEmail(String userEmail){
         Map<String,String> map = new HashMap<>();
-        PanyuUser panyuUser = a_registerPanyuUserService.selectUserEmail(userEmail);
-        if (panyuUser == null){
+        int count = a_registerPanyuUserService.selectUserEmail(userEmail);
+        if (count == 0){
             map.put("msg","1");
         }else {
             map.put("msg","2");
@@ -30,30 +31,31 @@ public class A_RegisterPanyuUserController {
         return map;
     }
 
-    /*@RequestMapping("insertPanyuUser")
-    public @ResponseBody PanyuUser insertPanyuUser(
-            String userProvince,
-            String userCity,
-            String userCounty,
-            String userCompanyName,
-            String userCompanycccNumber,
-            String userName,
-            String userEmail,
-            String userTel,
-            String userPwd,
-            String userState){
+    @RequestMapping("register")
+    @ResponseBody
+    public int userlogin(String userProvince, String userCity, String userCounty, String userCompanyName , String  userCompanycccNumber, String userName, String userEmail, String userPwd , String userTel){
+        // 将用户名作为盐值
+        ByteSource salt = ByteSource.Util.bytes(userName);
+        /* * MD5加密：
+         *   使用SimpleHash类对原始密码进行加密。
+         *  第一个参数代表使用MD5方式加密
+         *  第二个参数为原始密码 *
+         *  第三个参数为盐值，即用户名
+         * 第四个参数为加密次数 *
+         * 最后用toHex()方法将加密后的密码转成String
+         *  */
 
         int result = 0;
-        PanyuUser panyuUser = panyuUserDao.selectUserEmail(userEmail);
+        int count = a_registerPanyuUserService.selectUserEmail(userEmail);
+        System.out.println("数量:"+count);
+        String newPs = new SimpleHash("MD5", userPwd, salt, 1024).toHex();
 
-        result= panyuUserService.insertPanyuUser(userProvince, userCity, userCounty, userCompanyName, userCompanycccNumber, userName, userEmail, userTel, userPwd, userState);
-        if (result == 1){
+        if (count == 0){
+            result = a_registerPanyuUserService.register(userProvince,userCity,userCounty,userCompanyName,userCompanycccNumber,userName,userEmail,userTel,newPs);
             System.out.println("注册成功");
-        }
-
-        if (panyuUser !=null){
+        }else {
             System.out.println("该用户已经存在");
         }
-        return panyuUser;
-    }*/
+        return result;
+    }
 }
