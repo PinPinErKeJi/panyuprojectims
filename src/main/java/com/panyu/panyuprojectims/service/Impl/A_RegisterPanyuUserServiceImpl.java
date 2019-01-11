b9dc5e3f107bff8a22ad8b64363ce0d7272f16c6
xiaodu <py_dujianlei@163.com>
2019-01-11 15:34:33 +0800

    2019-1-11提交判断注册service实现类

--- src/main/java/com/panyu/panyuprojectims/service/Impl/A_RegisterPanyuUserServiceImpl.java
+++ src/main/java/com/panyu/panyuprojectims/service/Impl/A_RegisterPanyuUserServiceImpl.java
@@ -13,8 +13,35 @@ public class A_RegisterPanyuUserServiceImpl implements A_RegisterPanyuUserServic
     private A_RegisterPanyuUserDao a_registerPanyuUserDao;
 
     @Override
-    public PanyuUser selectUserEmail(String userEmail) {
+    public int selectUserEmail(String userEmail) {
         return a_registerPanyuUserDao.selectUserEmail(userEmail);
     }
 
+    @Override
+    public int register(
+            String userProvince,
+            String userCity,
+            String userCounty,
+            String userCompanyName,
+            String userCompanycccNumber,
+            String userName,
+            String userEmail,
+            String userTel,
+            String userPwd
+    ) {
+        int result = 0;
+        try {
+                result = a_registerPanyuUserDao.register(
+                        userProvince, userCity, userCounty,
+                        userCompanyName, userCompanycccNumber, userName, userEmail, userTel, userPwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public PanyuUser queryInfoByUsername(String userName) {
+        return a_registerPanyuUserDao.queryInfoByUsername(userName);
+    }
 }
