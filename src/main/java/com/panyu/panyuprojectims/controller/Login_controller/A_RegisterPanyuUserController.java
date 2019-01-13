e9caf2a2108c52cf382f8c7e8944459cf4932e28
xiaodu <py_dujianlei@163.com>
2019-01-13 09:19:14 +0800

    2019-1-13提交注册controller控制类

--- src/main/java/com/panyu/panyuprojectims/controller/Login_controller/A_RegisterPanyuUserController.java
+++ src/main/java/com/panyu/panyuprojectims/controller/Login_controller/A_RegisterPanyuUserController.java
@@ -18,11 +18,11 @@ public class A_RegisterPanyuUserController {
     @Autowired
     private A_RegisterPanyuUserService a_registerPanyuUserService;
 
-    @RequestMapping("selectUserEmail")
+    @RequestMapping("selectUserName")
     @ResponseBody
-    public Map selectUserEmail(String userEmail){
+    public Map selectUserName(String userName){
         Map<String,String> map = new HashMap<>();
-        int count = a_registerPanyuUserService.selectUserEmail(userEmail);
+        int count = a_registerPanyuUserService.selectUserName(userName);
         if (count == 0){
             map.put("msg","1");
         }else {
@@ -31,19 +31,19 @@ public class A_RegisterPanyuUserController {
         return map;
     }
 
-    @RequestMapping("register")
+    /*@RequestMapping("register")
     @ResponseBody
     public int userlogin(String userProvince, String userCity, String userCounty, String userCompanyName , String  userCompanycccNumber, String userName, String userEmail, String userPwd , String userTel){
         // 将用户名作为盐值
         ByteSource salt = ByteSource.Util.bytes(userName);
-        /* * MD5加密：
+        *//* * MD5加密：
          *   使用SimpleHash类对原始密码进行加密。
          *  第一个参数代表使用MD5方式加密
          *  第二个参数为原始密码 *
          *  第三个参数为盐值，即用户名
          * 第四个参数为加密次数 *
          * 最后用toHex()方法将加密后的密码转成String
-         *  */
+         *  *//*
         int result = 0;
         int count = a_registerPanyuUserService.selectUserEmail(userEmail);
         System.out.println("数量:"+count);
@@ -56,5 +56,5 @@ public class A_RegisterPanyuUserController {
             System.out.println("该用户已经存在");
         }
         return result;
-    }
+    }*/
 }
