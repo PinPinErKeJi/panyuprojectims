eb9ded00bee6d509dde7bd4118653f2bd49ac75c
yangqi <py_yangqi@163.com>
2019-01-13 18:27:35 +0800

    2019-1-13 修改login.js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -176,49 +176,49 @@ $(function () {
 
 
 //记住用户名密码
-window.onload = function(){
-    var oForm = document.getElementById('loginForm');
-    var oUser = document.getElementById('userName1');
-    var oPswd = document.getElementById('userPwd');
-    var oRemember = document.getElementById('remember');
-    //页面初始化时，如果帐号密码cookie存在则填充
-    if(getCookie('userName1') && getCookie('userPwd')){
-        oUser.value = getCookie('userName1');
-        oPswd.value = getCookie('userPwd');
-        oRemember.checked = true;
-    }
-    //复选框勾选状态发生改变时，如果未勾选则清除cookie
-    oRemember.onchange = function(){
-        if(!this.checked){
-            delCookie('userName1');
-            delCookie('userPwd');
-        }
-    };
-    //表单提交事件触发时，如果复选框是勾选状态则保存cookie
-    oForm.onsubmit = function(){
-        if(remember.checked){
-            setCookie('userName1',oUser.value,7); //保存帐号到cookie，有效期7天
-            setCookie('userPwd',oPswd.value,7); //保存密码到cookie，有效期7天
-        }
-    };
-};
-//设置cookie
-function setCookie(name,value,day){
-    var date = new Date();
-    date.setDate(date.getDate() + day);
-    document.cookie = name + '=' + value + ';expires='+ date;
-};
-//获取cookie
-function getCookie(name){
-    var reg = RegExp(name+'=([^;]+)');
-    var arr = document.cookie.match(reg);
-    if(arr){
-        return arr[1];
-    }else{
-        return '';
-    }
-};
-//删除cookie
-function delCookie(name){
-    setCookie(name,null,-1);
-};
+// window.onload = function(){
+//     var oForm = document.getElementById('loginForm');
+//     var oUser = document.getElementById('userName1');
+//     var oPswd = document.getElementById('userPwd');
+//     var oRemember = document.getElementById('remember');
+//     //页面初始化时，如果帐号密码cookie存在则填充
+//     if(getCookie('userName1') && getCookie('userPwd')){
+//         oUser.value = getCookie('userName1');
+//         oPswd.value = getCookie('userPwd');
+//         oRemember.checked = true;
+//     }
+//     //复选框勾选状态发生改变时，如果未勾选则清除cookie
+//     oRemember.onchange = function(){
+//         if(!this.checked){
+//             delCookie('userName1');
+//             delCookie('userPwd');
+//         }
+//     };
+//     //表单提交事件触发时，如果复选框是勾选状态则保存cookie
+//     oForm.onsubmit = function(){
+//         if(remember.checked){
+//             setCookie('userName1',oUser.value,7); //保存帐号到cookie，有效期7天
+//             setCookie('userPwd',oPswd.value,7); //保存密码到cookie，有效期7天
+//         }
+//     };
+// };
+// //设置cookie
+// function setCookie(name,value,day){
+//     var date = new Date();
+//     date.setDate(date.getDate() + day);
+//     document.cookie = name + '=' + value + ';expires='+ date;
+// };
+// //获取cookie
+// function getCookie(name){
+//     var reg = RegExp(name+'=([^;]+)');
+//     var arr = document.cookie.match(reg);
+//     if(arr){
+//         return arr[1];
+//     }else{
+//         return '';
+//     }
+// };
+// //删除cookie
+// function delCookie(name){
+//     setCookie(name,null,-1);
+// };
