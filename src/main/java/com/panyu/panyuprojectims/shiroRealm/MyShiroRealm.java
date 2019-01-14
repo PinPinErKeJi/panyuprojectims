c1913ff09d312567d3193ec54b346e986e78d853
pinpin <18671095993@163.com>
2019-01-14 12:05:00 +0800

    1.14

--- src/main/java/com/panyu/panyuprojectims/shiroRealm/MyShiroRealm.java
+++ src/main/java/com/panyu/panyuprojectims/shiroRealm/MyShiroRealm.java
@@ -4,6 +4,7 @@ import com.panyu.panyuprojectims.entity.PanyuUser;
 import com.panyu.panyuprojectims.service.PanyuUserService;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
@@ -15,10 +16,19 @@ import java.util.Map;
 public class MyShiroRealm extends AuthorizingRealm {
     @Autowired
     private PanyuUserService panyuUserService;
-    /**授权**/
+    /**授权
+     * 此方法调用  hasRole,hasPermission的时候才会进行回调.
+     *1、如果用户正常退出，缓存自动清空；
+     *2、 如果用户非正常退出，缓存自动清空；
+     *3、如果我们修改了用户的权限，而用户不退出系统，修改的权限无法立即生效。
+     * （需要手动编程进行实现；放在service进行调用）
+     * 在权限修改后调用realm中的方法，realm已经由spring管理，所以从spring中获取realm实例，
+     *
+
+     * **/
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        System.out.println("我是授权");
+        System.out.println("我是授权被调用");
         //获取当前登录的用户名
         String userName = (String) principals.fromRealm(getName()).iterator().next();
         //根据用户名，查询当前登录的用户所拥有的所有角色
@@ -31,7 +41,17 @@ public class MyShiroRealm extends AuthorizingRealm {
         for (String funName : funList) {
             System.out.println("fun----------------"+funName);
         }
-        return null;
+
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        //告诉shiro当前用户的角色
+        if(roleList!=null && roleList.size()>0){
+            info.addRoles(roleList);
+        }
+        //告诉shiro当前用户的权限
+        if(funList!=null && funList.size()>0){
+            info.addStringPermissions(funList);
+        }
+        return info;
 
     }
     /**认证**/
