aebacbfacaaefd9efc28094a85b9f1202f4cafed
zhangxiaoliang <py_zhangxiaoliang@163.com>
2019-01-14 18:46:41 +0800

    2019-01-14 修改PanyuUserDao 接口

--- src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
+++ src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
@@ -8,28 +8,24 @@ import java.util.Map;
 
 @Mapper
 public interface PanyuUserDao{
-
-    //查询所有的用户
-    List<PanyuUser> queryPanyuUserList(@Param("start")int start,@Param("end")int end);
-    //查询所有的用户个数据
-    long queryCountPanyuUser();
-
-
     public int register(   @Param("userProvince")String userProvince,
-                            @Param("userCity")String userCity,
-                            @Param("userCounty")String userCounty,
-                            @Param("userCompanyName")String userCompanyName,
-                            @Param("userCompanycccNumber")String userCompanycccNumber,
-                            @Param("userName")String userName,
-                            @Param("userEmail")String userEmail,
-                            @Param("userTel")String userTel,
-                            @Param("userPwd")String userPwd,
-                            @Param("userLogPwd")String userLogPwd
-                           );
+                           @Param("userCity")String userCity,
+                           @Param("userCounty")String userCounty,
+                           @Param("userCompanyName")String userCompanyName,
+                           @Param("userCompanycccNumber")String userCompanycccNumber,
+                           @Param("userName")String userName,
+                           @Param("userEmail")String userEmail,
+                           @Param("userTel")String userTel,
+                           @Param("userPwd")String userPwd
+    );
 
     public PanyuUser queryInfoByUsername(@Param("userName")String userName);
 
     public List<String> queryRolesByUsername(@Param("userName") String userName);
     public List<String> queryResByUsername(@Param("userName")String userName);
 
+    //查询所有的用户
+    List<PanyuUser> queryPanyuUserList(@Param("start")int start,@Param("end")int end);
+    //查询所有的用户个数据
+    long queryCountPanyuUser();
 }
