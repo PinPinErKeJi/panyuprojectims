f1621e7851d82f67d5ee810b0d15d5e4609ddf01
yangqi <py_yangqi@163.com>
2019-01-14 16:13:57 +0800

    2019-1-14 修改login.js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -156,38 +156,39 @@ $('.btn').on('click',function () {
 
 
 $(function () {
-   var register = document.getElementById('degister');
-   $('#register').on('click',function () {
-       for (var i = 0; i < $('#Reform')[0].elements.length - 1;i++){
-           if($('#Reform')[0].elements[i].value=="")
-           {
-               $('#Reform')[0].elements[i].focus();
-               return false;
-           }
-       }
-       return true;
-           var companyName=$("#companyName").val();
-           var userName=$("#userName").val();
-           var companyNumber=$("#companyNumber").val();
-           var email=$("#email").val();
-           var password=$("#password").val();
-           var tel=$("#tel").val();
-           var province=$("#province").val();
-           var city=$("#city").val();
-           var county=$("#county").val();
+    // var formData=new FormData($("#Reform")[0]);
+
+    $("#register").click( function () {
+
+        var companyName=$("#companyName").val();
+        var userName=$("#userName").val();
+        var companyNumber=$("#companyNumber").val();
+        var email=$("#email").val();
+        var password=$("#password").val();
+        var tel=$("#tel").val();
+        var province=$("#province").val();
+        var city=$("#city").val();
+        var county=$("#county").val();
+            if (companyName ==''||userName==''||companyNumber==''||email==''||password==''||tel==''
+            || !$('#Reform input :checked')
+            ){
+                    return false;
+            }else{
+                $.post("userLoginController/userlogin",
+                    {"userCompanyName":companyName,"userName":userName,"userCompanycccNumber":companyNumber,"userEmail":email,"userPwd":password,"userTel":tel,
+                        "userProvince":province,"userCity":city,"userCounty":county},
+                    function(msg){
+                        if(msg>0){
+                            alert("注册成功！请点击返回登录");
+                            window.location.href="login.html";
+                        }
+                    });
+                return true;
+            }
 
 
-           $.post("userLoginController/userlogin",
-               {"userCompanyName":companyName,"userName":userName,"userCompanycccNumber":companyNumber,"userEmail":email,"userPwd":password,"userTel":tel,
-               "userProvince":province,"userCity":city,"userCounty":county},
-               function(msg){
-                   if(msg>0){
-                       alert("注册成功！请点击返回登录");
-                       window.location.href="login.html";
-                   }
-           });
-   })
-    })
+    });
+});
 
 
 //记住用户名密码
