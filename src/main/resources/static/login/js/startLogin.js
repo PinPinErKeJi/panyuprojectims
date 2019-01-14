20d0985aa667b77e5b6e1f3a932880f3c46cae6c
pinpin <18671095993@163.com>
2019-01-14 19:53:24 +0800

    1.14

--- src/main/resources/static/login/js/startLogin.js
+++ src/main/resources/static/login/js/startLogin.js
@@ -3,8 +3,9 @@ $(function () {
     $("#loginbutton").click(function () {
         var userName1=$("#userName1").val();
         var userPwd=$("#userPwd").val();
+        var rememberMe=$('#idcheckbox').is(':checked')//返回true/false
 
-        $.post("userLoginController/startlogin",{"userName":userName1,"userPwd":userPwd},function (msg) {
+        $.post("userLoginController/startlogin",{"userName":userName1,"userPwd":userPwd,"rememberMe":rememberMe},function (msg) {
                 if(msg==1){
                     alert("对不起用户名错误")
                 }else if(msg==2){
