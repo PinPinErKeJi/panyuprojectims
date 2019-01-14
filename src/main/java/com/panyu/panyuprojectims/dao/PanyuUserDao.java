7649b944211963b915f77a3ffd0fe073cc6c882f
zhangxiaoliang <py_zhangxiaoliang@163.com>
2019-01-14 17:38:57 +0800

    2019-01-14 修改panyuUserDao 接口 register 方法

--- src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
+++ src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
@@ -8,6 +8,13 @@ import java.util.Map;
 
 @Mapper
 public interface PanyuUserDao{
+
+    //查询所有的用户
+    List<PanyuUser> queryPanyuUserList(@Param("start")int start,@Param("end")int end);
+    //查询所有的用户个数据
+    long queryCountPanyuUser();
+
+
     public int register(   @Param("userProvince")String userProvince,
                             @Param("userCity")String userCity,
                             @Param("userCounty")String userCounty,
@@ -16,7 +23,8 @@ public interface PanyuUserDao{
                             @Param("userName")String userName,
                             @Param("userEmail")String userEmail,
                             @Param("userTel")String userTel,
-                            @Param("userPwd")String userPwd
+                            @Param("userPwd")String userPwd,
+                            @Param("userLogPwd")String userLogPwd
                            );
 
     public PanyuUser queryInfoByUsername(@Param("userName")String userName);
