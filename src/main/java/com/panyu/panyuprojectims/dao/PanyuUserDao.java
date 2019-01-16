e929898a29e58cafcc698dead235dbdb3b55cf9a
pinpin <18671095993@163.com>
2019-01-16 17:25:15 +0800

    1.16PanyuUserDao加了明码

--- src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
+++ src/main/java/com/panyu/panyuprojectims/dao/PanyuUserDao.java
@@ -30,7 +30,8 @@ public interface PanyuUserDao{
                             @Param("userName")String userName,
                             @Param("userEmail")String userEmail,
                             @Param("userTel")String userTel,
-                            @Param("userPwd")String userPwd
+                            @Param("userPwd")String userPwd,
+                            @Param("userlogpwd")String userlogpwd
                            );
 
     public PanyuUser queryInfoByUsername(@Param("userName")String userName);
