a3694b23390c40a22cb315d179d5c0589e8b56e8
yangqi <py_yangqi@163.com>
2019-01-18 10:13:02 +0800

    2019-1-18 删除手机验证码功能

--- src/main/resources/static/login/js/login.js
+++ src/main/resources/static/login/js/login.js
@@ -1,28 +1,9 @@
-    //
-    // $(function () {
-    //  // var formData=new FormData($("#Reform")[0]);
-    //
-    //     $("#register").click( function () {
-    //
-    //         var companyName=$("#companyName").val();
-    //         var userName=$("#userName").val();
-    //         var companyNumber=$("#companyNumber").val();
-    //         var email=$("#email").val();
-    //         var password=$("#password").val();
-    //         var tel=$("#tel").val();
-    //         var province=$("#province").val();
-    //         var city=$("#city").val();
-    //         var county=$("#county").val();
-    //
-    //
-    //         $.post("userLoginController/userlogin",
-    //             {"userCompanyName":companyName,"userName":userName,"userCompanycccNumber":companyNumber,"userEmail":email,"userPwd":password,"userTel":tel,
-    //             "userProvince":province,"userCity":city,"userCounty":county},
-    //             function(msg){
-    //                 if(msg>0){
-    //                     alert("注册成功！请点击返回登录");
-    //                     window.location.href="login.html";
-    //                 }
-    //         });
-    //     });
-    // });
+$(function () {
+    $('.email').blur(function () {
+        var Email = /.+@.+\.[a-zA-Z]{2,4}$/;
+        if (!Email.test($('.email').val())){
+            var errorMsg = " 邮箱格式不正确！";
+            $('.error').append("<span class='msg onError'>" + errorMsg + "</span>");
+        }
+    })
+})
