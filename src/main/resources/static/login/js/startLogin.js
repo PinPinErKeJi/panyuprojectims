d10da8f6e881c8aa1bcaa7a690a9e8e4f248242b
pinpin <18671095993@163.com>
2019-01-25 20:22:50 +0800

    1.25提示信息

--- src/main/resources/static/login/js/startLogin.js
+++ src/main/resources/static/login/js/startLogin.js
@@ -5,16 +5,20 @@ $(function () {
         var userPwd=$("#userPwd").val();
         var rememberMe=$('#idcheckbox').is(':checked')//返回true/false
 
-        $.post("userLoginController/startlogin",{"userName":userName1,"userPwd":userPwd,"rememberMe":rememberMe},function (msg) {
-
-            if(msg==""){
-                sessionStorage.setItem("userName",userName1)
-                window.location.href="index.html";
-            }else{
-                alert(msg)
-            }
-        });
-
+        if(userName1 == "" && userPwd == ""){
+            $('#infoSpan').text("请输入用户名和密码")
+        }else {
+            $.post("userLoginController/startlogin",{"userName":userName1,"userPwd":userPwd,"rememberMe":rememberMe},function (msg) {
+                if(msg==""){
+                    sessionStorage.setItem("userName",userName1)
+                    window.location.href="index.html";
+                }else{
+                    //alert(msg)
+                    $('#infoSpan').text(msg)
+                }
+            });
+        }
     })
+
 
 })
