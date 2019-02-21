a701be8357f8dcf4fb370838a538043af7c89ee7
yangqi <py_yangqi@163.com>
2019-02-21 11:29:09 +0800

    修改index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,21 +1,14 @@
-$(function(){
-    $('.personal').tooltip({
-        hideEvent: 'none',
-        showEvent:'click',
-        content: function(){
-            return $('.message');
-        },
-        onUpdate: function(content){
-            content.panel({
-                width: 160,
-                border: false,
-            });
-        },
-        onShow: function(){
-            var t = $(this);
-            t.tooltip('tip').focus().unbind().bind('click',function(){
-                t.tooltip('hide');
-            });
+$(function () {
+    $('.login-info-one').click(function () {
+        if($(".message").css('display')=='none'){
+            $(".message").css("display","block");
+        }else {
+            $(".message").css("display","none");
         }
-    });
-});
+    })
+    $('body').click(function () {
+        if($(".message").css('display')=='block'){
+            $(".message").css("display","none");
+        }
+    })
+})
