7dd05f08cb354ed0c3b06d1d7fcbbf8882b5d94a
pinpin <18671095993@163.com>
2019-02-24 14:06:22 +0800

    关键工序到位controller提交

---
+++ src/main/java/com/panyu/panyuprojectims/controller/Date_analyze_controller/criticalController/criticalDateController.java
@@ -0,0 +1,11 @@
+package com.panyu.panyuprojectims.controller.Date_analyze_controller.criticalController;
+
+import org.springframework.stereotype.Controller;
+
+/*
+    关键工序到为率
+ */
+@Controller
+public class criticalDateController {
+
+}
